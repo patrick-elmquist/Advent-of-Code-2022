d7a344bc833dc4d84313923d63fae187683ef2f7
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2022-12-03 06:41:19 +0100

    Day 3 dirty solution

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Advent of Code 2022 🎄🌟🎅
 Solutions for Advent of Code 2022 written in Kotlin
 
-Solved: Day 1-2
+Solved: Day 1-3
 
 [Link to Solutions](https://github.com/patrick-elmquist/Advent-of-Code-2022/tree/main/src/main/kotlin)
--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -6,9 +6,40 @@ import day.day
 fun main() {
     day(n = 3) {
         part1 { input ->
+            input.lines.map {
+                val (first, second) = it.chunked(it.length / 2).map { it.toSet() }
+                println(first)
+                println(second)
+                val union = first.intersect(second).single()
+                if (union.isUpperCase()) {
+                    union - 'A' + 1 + 26
+                } else {
+                    union - 'a' + 1
+                }
+            }
+                .sum()
         }
 
+        val testInput = """
+            vJrwpWtwJgWrhcsFMMfFFhFp
+            jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+            PmmdzqPrVvPwwTWBwg
+            wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+            ttgJtRGJQctTZtZT
+            CrZsJsPPZsGzwwsLwLmpwMDw""".trimIndent()
+
+        part1 verify testInput expect 157
         part2 { input ->
+            input.lines.chunked(3) {
+                val (first, second, third) = it.map { it.toSet() }
+                val union = first.intersect(second).intersect(third).single()
+                if (union.isUpperCase()) {
+                    union - 'A' + 1 + 26
+                } else {
+                    union - 'a' + 1
+                }
+            }.sum()
         }
+        part2 verify testInput expect 70
     }
 }
