f329496ee7eb6edfb2c2ca66fc54109624354e0e
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2022-12-03 06:50:39 +0100

    Day 3 cleanup

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -1,45 +1,24 @@
 import day.day
 
-// answer #1:
-// answer #2:
+// answer #1: 7763
+// answer #2: 2569
 
 fun main() {
     day(n = 3) {
-        part1 { input ->
-            input.lines.map {
-                val (first, second) = it.chunked(it.length / 2).map { it.toSet() }
-                println(first)
-                println(second)
-                val union = first.intersect(second).single()
-                if (union.isUpperCase()) {
-                    union - 'A' + 1 + 26
-                } else {
-                    union - 'a' + 1
-                }
+        part1(expected = 7763) { input ->
+            input.lines.sumOf { line ->
+                val (left, right) = line.chunked(line.length / 2).map { it.toSet() }
+                left.intersect(right).single().priority
             }
-                .sum()
         }
-
-        val testInput = """
-            vJrwpWtwJgWrhcsFMMfFFhFp
-            jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-            PmmdzqPrVvPwwTWBwg
-            wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-            ttgJtRGJQctTZtZT
-            CrZsJsPPZsGzwwsLwLmpwMDw""".trimIndent()
-
-        part1 verify testInput expect 157
-        part2 { input ->
-            input.lines.chunked(3) {
-                val (first, second, third) = it.map { it.toSet() }
-                val union = first.intersect(second).intersect(third).single()
-                if (union.isUpperCase()) {
-                    union - 'A' + 1 + 26
-                } else {
-                    union - 'a' + 1
-                }
+        part2(expected = 2569) { input ->
+            input.lines.chunked(3) { group ->
+                val (first, second, third) = group.map { line -> line.toSet() }
+                first.intersect(second).intersect(third).single().priority
             }.sum()
         }
-        part2 verify testInput expect 70
     }
 }
+
+private val Char.priority: Int
+    get() = 1 + if (isUpperCase()) this - 'A' + 26 else this - 'a'
