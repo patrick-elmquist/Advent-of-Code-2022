042ff4fe1341d9cb7ca89eddf4c60d015775e5e5
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2022-12-05 12:53:11 +0100

    Day 5 starting to look better, but stack parsing is still a mess

--- src/main/kotlin/Day5.kt
+++ src/main/kotlin/Day5.kt
@@ -1,3 +1,4 @@
+import day.Input
 import day.day
 import util.groupByBlank
 
@@ -7,59 +8,70 @@ import util.groupByBlank
 fun main() {
     day(n = 5) {
         part1(expected = "PTWLTDSJV") { input ->
-            val (top, bottom) = input.lines.groupByBlank()
-            val (stacks, instructions) = top.toStacks() to bottom.toInstructions()
-            stacks.rearrange(instructions, reverseOrder = true)
+            input.parseInstructions()
+                .fold(input.parseStacks()) { stacks, instruction ->
+                    stacks.move(instruction, reverseOrder = true)
+                }
+                .output()
         }
 
         part2(expected = "WZMFVGGZP") { input ->
-            val (top, bottom) = input.lines.groupByBlank()
-            val (stacks, instructions) = top.toStacks() to bottom.toInstructions()
-            stacks.rearrange(instructions, reverseOrder = false)
+            input.parseInstructions()
+                .fold(input.parseStacks()) { stacks, instruction ->
+                    stacks.move(instruction, reverseOrder = false)
+                }
+                .output()
         }
     }
 }
 
-private fun Stacks.rearrange(
-    instructions: List<Instruction>,
-    reverseOrder: Boolean
-): String {
-    instructions.forEach { (count, from, to) ->
-        val end = get(from).size
-        val items = get(from).subList(end - count, end).toMutableList()
-        repeat(count) {
-            get(from).removeLast()
+private fun Input.parseInstructions(): List<Instruction> =
+    lines.groupByBlank().let { (_, bottom) ->
+        val pattern = """move (\d+) from (\d+) to (\d+)""".toRegex()
+        bottom.map { instruction ->
+            val (c, f, t) = pattern.matchEntire(instruction)?.destructured
+                ?: error("regex failed to match content for $instruction")
+            Instruction(c.toInt(), f.toInt() - 1, t.toInt() - 1)
         }
-        if (reverseOrder) items.reverse()
-        get(to).addAll(items)
     }
-    return map { it.last() }.joinToString("")
-}
 
-private fun List<String>.toStacks(): Stacks {
-    val stackCount = last().trim().split("\\s+".toRegex()).count()
-    return dropLast(1)
-        .reversed()
-        .fold(MutableList(stackCount) { mutableListOf() }) { stack, line ->
-            stack.apply {
-                line.withIndex()
-                    .drop(1)
-                    .windowed(size = 1, step = 4) { it.first() }
-                    .filter { it.value != ' ' }
-                    .forEach { (index, char) -> get(index / 4).add(char) }
-            }
-        }
-}
-
-private fun List<String>.toInstructions(): List<Instruction> {
-    val pattern = """move (\d+) from (\d+) to (\d+)""".toRegex()
-    return map { instruction ->
-        val (c, f, t) = pattern.matchEntire(instruction)?.destructured
-            ?: error("regex failed to match content for $instruction")
-        Instruction(c.toInt(), f.toInt() - 1, t.toInt() - 1)
+private fun Input.parseStacks(): Stacks =
+    lines.groupByBlank().let { (top, _) ->
+        val stacks = top.last().trim().split("\\s+".toRegex()).count()
+        top.dropLast(1)
+            .reversed()
+            .fold<String, List<MutableList<Char>>>(List(stacks) { mutableListOf() }) { stack, line ->
+                stack.apply {
+                    line.withIndex()
+                        .drop(1)
+                        .windowed(size = 1, step = 4) { it.first() }
+                        .filter { it.value != ' ' }
+                        .forEach { (index, char) -> get(index / 4).add(char) }
+                }
+            }.let { Stacks(it) }
     }
-}
-
-private typealias Stacks = List<MutableList<Char>>
 
 private data class Instruction(val count: Int, val from: Int, val to: Int)
+
+private data class Stacks(val stacks: List<List<Char>>) {
+    fun move(instruction: Instruction, reverseOrder: Boolean): Stacks {
+        val (count, indexFrom, indexTo) = instruction
+        val from = stacks[indexFrom]
+        val to = stacks[indexTo]
+
+        val itemsToMove = if (reverseOrder) {
+            from.takeLast(count).reversed()
+        } else {
+            from.takeLast(count)
+        }
+
+        return Stacks(
+            stacks.toMutableList().apply {
+                set(indexFrom, from.dropLast(count))
+                set(indexTo, to + itemsToMove)
+            }
+        )
+    }
+
+    fun output() = stacks.map { it.last() }.joinToString("")
+}
