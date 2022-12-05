9bdf7c69e3396f891b68895745594152907b2017
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2022-12-05 11:25:18 +0100

    Day 5 better again, still not pleased

--- src/main/kotlin/Day5.kt
+++ src/main/kotlin/Day5.kt
@@ -1,4 +1,3 @@
-import day.Input
 import day.day
 import util.groupByBlank
 
@@ -8,65 +7,59 @@ import util.groupByBlank
 fun main() {
     day(n = 5) {
         part1(expected = "PTWLTDSJV") { input ->
-            val (stacks, instructions) = input.parse()
-            instructions
-                .fold(stacks) { output, (count, from ,to) ->
-                    output.apply {
-                        repeat(count) {
-                            get(to).add(get(from).removeLast())
-                        }
-                    }
-                }.output()
+            val (top, bottom) = input.lines.groupByBlank()
+            val (stacks, instructions) = top.toStacks() to bottom.toInstructions()
+            stacks.rearrange(instructions, reverseOrder = true)
         }
 
         part2(expected = "WZMFVGGZP") { input ->
-            val (stacks, instructions) = input.parse()
-            instructions
-                .fold(stacks) { output, (count, from, to) ->
-                    output.apply {
-                        val stackFrom = get(from)
-                        val end = stackFrom.size
-                        val items = stackFrom.subList(end - count, end).toList()
-                        repeat(count) {
-                            stackFrom.removeLast()
-                        }
-                        get(to).addAll(items)
-                    }
-                }.output()
+            val (top, bottom) = input.lines.groupByBlank()
+            val (stacks, instructions) = top.toStacks() to bottom.toInstructions()
+            stacks.rearrange(instructions, reverseOrder = false)
         }
     }
 }
 
-private fun Input.parse(): Pair<Stacks, List<Instruction>> {
-    val (stacksInput, instructionsInput) = lines.groupByBlank()
-    return stacksInput.toStacks() to instructionsInput.toInstructions()
+private fun Stacks.rearrange(
+    instructions: List<Instruction>,
+    reverseOrder: Boolean
+): String {
+    instructions.forEach { (count, from, to) ->
+        val end = get(from).size
+        val items = get(from).subList(end - count, end).toMutableList()
+        repeat(count) {
+            get(from).removeLast()
+        }
+        if (reverseOrder) items.reverse()
+        get(to).addAll(items)
+    }
+    return map { it.last() }.joinToString("")
 }
 
 private fun List<String>.toStacks(): Stacks {
-    val cols = last().trim().split("\\s+".toRegex()).count()
+    val stackCount = last().trim().split("\\s+".toRegex()).count()
     return dropLast(1)
         .reversed()
-        .fold(MutableList(cols) { mutableListOf() }) { stack, line ->
-            var index = 1
-            while (index < line.length) {
-                if (line[index] != ' ') {
-                    stack[index / 4].add(line[index])
-                }
-                index += 4
+        .fold(MutableList(stackCount) { mutableListOf() }) { stack, line ->
+            stack.apply {
+                line.withIndex()
+                    .drop(1)
+                    .windowed(size = 1, step = 4) { it.first() }
+                    .filter { it.value != ' ' }
+                    .forEach { (index, char) -> get(index / 4).add(char) }
             }
-            stack
         }
 }
 
 private fun List<String>.toInstructions(): List<Instruction> {
     val pattern = """move (\d+) from (\d+) to (\d+)""".toRegex()
     return map { instruction ->
-        val (c, f, t) = pattern.matchEntire(instruction)?.destructured!!
+        val (c, f, t) = pattern.matchEntire(instruction)?.destructured
+            ?: error("regex failed to match content for $instruction")
         Instruction(c.toInt(), f.toInt() - 1, t.toInt() - 1)
     }
 }
 
 private typealias Stacks = List<MutableList<Char>>
 
-private fun Stacks.output() = map { it.last() }.joinToString("")
 private data class Instruction(val count: Int, val from: Int, val to: Int)
