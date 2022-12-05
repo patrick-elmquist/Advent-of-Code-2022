18983a81673046a41203e6a6c507f07d4636086d
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2022-12-05 06:43:32 +0100

    Day 5 done but reeeeeeeally hacky solution, need cleanup

--- src/main/kotlin/Day5.kt
+++ src/main/kotlin/Day5.kt
@@ -4,7 +4,7 @@ import day.day
 import util.groupByBlank
 
 // answer #1: PTWLTDSJV
-// answer #2:
+// answer #2: WZMFVGGZP
 
 fun main() {
     day(n = 5) {
@@ -50,8 +50,40 @@ fun main() {
             |move 2 from 2 to 1
             |move 1 from 1 to 2 """.trimMargin("|")
         part1 verify testInput expect "CMZ"
-        part2 {
 
+        part2(expected = "WZMFVGGZP") {
+            val (top, instructions) = it.lines.groupByBlank()
+
+            val cols = top.last().trim().split("\\s+".toRegex()).count()
+            val list = List<MutableList<Char>>(cols) { mutableListOf() }
+
+            top.dropLast(1)
+                .forEach { line ->
+                    line.withIndex().filter {
+                        it.index in listOf(1, 5, 9, 13, 17, 21, 25, 29, 33, 37)
+                    }.map { it.value }.withIndex().filter { it.value != ' ' }.onEach{ it.log() }
+                        .forEach {
+                            list.get((it.index).coerceAtLeast(0)).add(it.value)
+                        }
+                }
+
+            list.log()
+            instructions.forEach {
+                val split = it.split(" ")
+                val count = split[1].toInt()
+                val from = split[3].toInt() - 1
+                val to = split[5].toInt() - 1
+
+                val items = list.get(from).subList(0, count).toList().log()
+                repeat(count) {
+                    list.get(from).removeFirst()
+                }
+                list.get(to).addAll(0, items)
+                list.log()
+            }
+            list.log()
+            list.map { it.first() }.joinToString("")
         }
+        part2 verify testInput expect "MCD"
     }
 }
