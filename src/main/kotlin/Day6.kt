da60f26111e7e69d1a42f2c256dba2a52a806572
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2022-12-06 06:15:20 +0100

    Day 6 Cleanup

--- src/main/kotlin/Day6.kt
+++ src/main/kotlin/Day6.kt
@@ -1,4 +1,3 @@
-
 import day.day
 import day.single
 
@@ -8,26 +7,17 @@ import day.single
 fun main() {
     day(n = 6) {
         part1 { input ->
-            input.single()
-                .windowed(4, 1) { it.toSet() }
-                .withIndex()
-                .first { it.value.size == 4 }
-                .index + 4
-
+            input.single().findMarker(n = 4)
         }
-        part1 test 1 expect 7
-        part1 test 2 expect 5
-        part1 test 3 expect 6
 
         part2 { input ->
-            input.single().windowed(14, 1) { it.toSet() }
-            .withIndex()
-            .first { it.value.size == 14 }
-            .index + 14
+            input.single().findMarker(n = 14)
         }
-        part2 test 1 expect 19
-        part2 test 2 expect 23
-        part2 test 3 expect 23
     }
 }
 
+private fun String.findMarker(n: Int): Int =
+    windowed(n, 1) { it.toSet() }
+        .withIndex()
+        .first { it.value.size == n }
+        .index + n
