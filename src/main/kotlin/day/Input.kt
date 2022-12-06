4bb9364337d6629509c1a8dd68c37e5734ff6a22
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2022-12-06 09:56:48 +0100

    Moved input and test files to resources

--- src/main/kotlin/day/Input.kt
+++ src/main/kotlin/day/Input.kt
@@ -7,9 +7,19 @@ import java.io.File
 
 data class Input(val lines: List<String>) {
     constructor(string: String) : this(string.split("\n"))
-    constructor(day: Int) : this(File("./assets/input-day-$day.txt"))
-    constructor(day: Int, test: Int) : this(File("./assets/tests/test-day-$day-$test.txt"))
+    constructor(day: Int) : this(getInputFile(day = day))
+    constructor(day: Int, test: Int) : this(getTestFile(day = day, n = test))
     constructor(file: File) : this(file.readLines())
+
+    companion object {
+        private const val RESOURCES_PATH = "./src/main/resources/"
+
+        private fun getInputFile(day: Int): File =
+            File("$RESOURCES_PATH/input-day-$day.txt")
+
+        private fun getTestFile(day: Int, n: Int): File =
+            File("$RESOURCES_PATH/tests/test-day-$day-$n.txt")
+    }
 }
 
 fun Input.toInts() = lines.toInts()
