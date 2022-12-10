6df22740ed7f0457432d1036c46b92aa5a95c086
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2022-12-10 07:47:04 +0100

    Cleanup of Day 10

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -18,27 +18,25 @@ fun main() {
             var x = 1
             var divider = 20
             val points = mutableListOf<Int>()
+
+            fun addPointIfNeeded() {
+                if (cycles % divider == 0) {
+                    points.add(cycles * x)
+                    divider += 40
+                }
+            }
             instructions.forEach { instruction ->
                 when (instruction.first()) {
                     "noop" -> {
                         cycles++
-                        if (cycles % divider == 0) {
-                            points.add(cycles * x)
-                            divider += 40
-                        }
+                        addPointIfNeeded()
                     }
                     else -> {
                         cycles ++
-                        if (cycles % divider == 0) {
-                            points.add(cycles * x)
-                            divider += 40
-                        }
+                        addPointIfNeeded()
                         cycles++
                         x += instruction.last().toInt()
-                        if (cycles % divider == 0) {
-                            points.add(cycles * x)
-                            divider += 40
-                        }
+                        addPointIfNeeded()
                     }
                 }
             }
@@ -51,13 +49,12 @@ fun main() {
 
             var cycles = 0
             var x = 1
-            val image = Array(6) { Array(40) { "@" } }
+            val image = Array(6) { Array(40) { '.' } }
             instructions.forEach { instruction ->
                 cycles++
                 image.draw(cycles, x)
                 when (instruction.first()) {
-                    "noop" -> {
-                    }
+                    "noop" -> Unit
                     else -> {
                         cycles++
                         image.draw(cycles, x)
@@ -70,28 +67,19 @@ fun main() {
     }
 }
 
-typealias Image = Array<Array<String>>
+private typealias Image = Array<Array<Char>>
 
-private fun Image.draw(pixel: Int, x: Int) {
-    val cycles = pixel - 1
-    val width = 40
-    val image = this
-    if ((cycles % width) in (x - 1..x + 1)) {
-        val i = cycles / width
-        val j = cycles % width
-        image[i][j] = "#"
-    } else {
-        val i = cycles / width
-        val j = cycles % width
-        image[i][j] = "."
-    }
+private fun Image.draw(cycles: Int, x: Int) {
+    val pixel = cycles - 1
+    val width = first().size
+    val i = pixel / width
+    val j = pixel % width
+    this[i][j] = if (j in (x - 1..x + 1)) '#' else '.'
 }
+
 private fun Image.print() {
     forEach { line ->
-        line.forEach {  item ->
-            print(item)
-        }
+        line.forEach {  pixel -> print(pixel) }
         println()
     }
-    println()
 }
