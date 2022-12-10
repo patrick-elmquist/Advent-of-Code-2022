de8cf279b2cdfb69b567e8b86925217778bb82b0
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2022-12-10 07:30:30 +0100

    Day 10 done, real messy

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -1,18 +1,97 @@
 import day.day
 
-// answer #1:
+// answer #1: 17180
 // answer #2:
+// ###..####.#..#.###..###..#....#..#.###..
+// #..#.#....#..#.#..#.#..#.#....#..#.#..#.
+// #..#.###..####.#..#.#..#.#....#..#.###..
+// ###..#....#..#.###..###..#....#..#.#..#.
+// #.#..#....#..#.#....#.#..#....#..#.#..#.
+// #..#.####.#..#.#....#..#.####..##..###..
 
 fun main() {
     day(n = 10) {
-        part1 { input ->
+        part1(expected = 17180) { input ->
+            val instructions = input.lines.map { it.split(" ") }
 
+            var cycles = 1
+            var x = 1
+            var divider = 20
+            val points = mutableListOf<Int>()
+            instructions.forEach { instruction ->
+                when (instruction.first()) {
+                    "noop" -> {
+                        cycles++
+                        if (cycles % divider == 0) {
+                            points.add(cycles * x)
+                            divider += 40
+                        }
+                    }
+                    else -> {
+                        cycles ++
+                        if (cycles % divider == 0) {
+                            points.add(cycles * x)
+                            divider += 40
+                        }
+                        cycles++
+                        x += instruction.last().toInt()
+                        if (cycles % divider == 0) {
+                            points.add(cycles * x)
+                            divider += 40
+                        }
+                    }
+                }
+            }
+
+            points.sum()
         }
-        part1 test 1 expect Unit
 
         part2 { input ->
+            val instructions = input.lines.map { it.split(" ") }
 
+            var cycles = 0
+            var x = 1
+            val image = Array(6) { Array(40) { "@" } }
+            instructions.forEach { instruction ->
+                cycles++
+                image.draw(cycles, x)
+                when (instruction.first()) {
+                    "noop" -> {
+                    }
+                    else -> {
+                        cycles++
+                        image.draw(cycles, x)
+                        x += instruction.last().toInt()
+                    }
+                }
+            }
+            image.print()
         }
-        part2 test 1 expect Unit
     }
+}
+
+typealias Image = Array<Array<String>>
+
+private fun Image.draw(pixel: Int, x: Int) {
+    val cycles = pixel - 1
+    val width = 40
+    val image = this
+    if ((cycles % width) in (x - 1..x + 1)) {
+        val i = cycles / width
+        val j = cycles % width
+        image[i][j] = "#"
+    } else {
+        val i = cycles / width
+        val j = cycles % width
+        image[i][j] = "."
+    }
+}
+private fun Image.print() {
+    forEach { line ->
+        line.forEach {  item ->
+            print(item)
+        }
+        println()
+    }
+    println()
 }
