6949e475759b1815803922437001d48b3aeaacc9
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2022-12-11 10:52:32 +0100

    Day 11 cut duration by half

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -8,20 +8,26 @@ import util.sliceByBlank
 fun main() {
     day(n = 11) {
         part1(expected = 57348L) { input ->
-            input.parseMonkeys().solve(rounds = 20, mapLevel = { it / 3 })
+            input.parseMonkeys().resolveThrows(
+                rounds = 20,
+                adjustWorryLevel = { it / 3 }
+            )
         }
         part1 test 1 expect 10605L
 
         part2(expected = 14106266886L) { input ->
             val monkeys = input.parseMonkeys()
             val lcm = monkeys.map { it.divisibleBy }.reduce(Long::times)
-            input.parseMonkeys().solve(rounds = 10_000, mapLevel = { it % lcm })
+            monkeys.resolveThrows(
+                rounds = 10_000,
+                adjustWorryLevel = { it % lcm }
+            )
         }
         part2 test 1 expect 2713310158L
     }
 }
 
-private fun Input.parseMonkeys(): List<Monkey> = lines
+private fun Input.parseMonkeys(): Array<Monkey> = lines
     .map(String::trim)
     .sliceByBlank()
     .mapIndexed { index, lines ->
@@ -31,38 +37,38 @@ private fun Input.parseMonkeys(): List<Monkey> = lines
                 .split(", ")
                 .map(String::toLong)
                 .toMutableList(),
-            operation = lines[2].removePrefix("Operation: new = old "),
+            operation = lines[2].removePrefix("Operation: new = old ").split(" ").first(),
+            operationValue = lines[2].removePrefix("Operation: new = old ").split(" ").last(),
             divisibleBy = lines[3].substringAfterLast(" ").toLong(),
             monkeyOnTrue = lines[4].substringAfterLast(" ").toInt(),
             monkeyOnFalse = lines[5].substringAfterLast(" ").toInt()
         )
     }
+    .toTypedArray()
 
-private fun List<Monkey>.solve(rounds: Int, mapLevel: (Long) -> Long): Long {
-    val monkeys = this
-    val counter = monkeys.associate { it.id to 0L }.toMutableMap()
+private fun Array<Monkey>.resolveThrows(rounds: Int, adjustWorryLevel: (Long) -> Long): Long {
+    val counter = Array(size) { 0L }
     repeat(rounds) {
-        monkeys.forEach { monkey ->
-            counter[monkey.id] = counter.getValue(monkey.id) + monkey.items.size
+        forEach { monkey ->
+            counter[monkey.id] += monkey.items.size.toLong()
             monkey.items.forEach { level ->
-                val (op, value) = monkey.operation.split(" ")
-                var newLevel = if (value == "old") {
-                    calculateWorryLevel(op, level, level)
+                var newLevel = if (monkey.operationValue == "old") {
+                    calculateWorryLevel(monkey.operation, level, level)
                 } else {
-                    calculateWorryLevel(op, level, value.toLong())
+                    calculateWorryLevel(monkey.operation, level, monkey.operationValue.toLong())
                 }
-                newLevel = mapLevel(newLevel)
+                newLevel = adjustWorryLevel(newLevel)
                 val nextMonkey = if (newLevel % monkey.divisibleBy == 0L) {
                     monkey.monkeyOnTrue
                 } else {
                     monkey.monkeyOnFalse
                 }
-                monkeys[nextMonkey].items += newLevel
+                get(nextMonkey).items += newLevel
             }
             monkey.items.clear()
         }
     }
-    return counter.values.sortedDescending().take(2).reduce(Long::times)
+    return counter.sortedDescending().take(2).reduce(Long::times)
 }
 
 private fun calculateWorryLevel(op: String, a: Long, b: Long): Long =
@@ -76,6 +82,7 @@ data class Monkey(
     val id: Int,
     val items: MutableList<Long> = mutableListOf(),
     val operation: String,
+    val operationValue: String,
     val divisibleBy: Long,
     val monkeyOnTrue: Int,
     val monkeyOnFalse: Int
