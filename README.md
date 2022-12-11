1b073b9c210d545941d048858c1ee949b6e11337
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2022-12-11 08:40:38 +0100

    Updated readme and prepared for Day 12

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Advent of Code 2022 🎄🌟🎅
 Solutions for Advent of Code 2022 written in Kotlin
 
-Solved: Day 1-10
+Solved: Day 1-11
 
 [Link to Solutions](https://github.com/patrick-elmquist/Advent-of-Code-2022/tree/main/src/main/kotlin)
---
+++ src/main/kotlin/Day12.kt
@@ -0,0 +1,18 @@
+import day.day
+
+// answer #1:
+// answer #2:
+
+fun main() {
+    day(n = 12) {
+        part1 { input ->
+
+        }
+        part1 test 1 expect Unit
+
+        part2 { input ->
+
+        }
+        part2 test 1 expect Unit
+    }
+}
