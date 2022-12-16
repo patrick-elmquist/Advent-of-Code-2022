b8aec0ae152f85bf9636e820a9908630d8c2163c
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2022-12-16 15:08:00 +0100

    Day 16 Part 2 done but slow and messy

--- src/main/kotlin/Day16.kt
+++ src/main/kotlin/Day16.kt
@@ -4,7 +4,7 @@ import util.log
 import java.util.*
 
 // answer #1: 1701
-// answer #2:
+// answer #2: not 2654, too high
 
 fun main() {
     day(n = 16) {
@@ -29,10 +29,76 @@ fun main() {
         part1 test 1 expect 1651
 
         part2 { input ->
+            val parsed = input.parse()
+            val map = parsed.associateBy { it.name }
+            val current = map.getValue("AA")
 
+            val cache = parsed.flatMap { a -> a.edge.map { b -> key(a.name, b) to 1 } }
+                .distinct()
+                .toMap()
+                .toMutableMap()
+
+            map.values.forEach { a ->
+                map.values.forEach { b ->
+                    map.shortestPath(a, b, cache)
+                }
+            }
+
+//            val me = Player("me", current, 26)
+//            val elephant = Player("elephant", current, 26)
+
+            val listOfLists = create((map.values.toList() - current).filter { it.rate > 0 })
+            var cache2 = mutableMapOf<Int, Int>()
+            listOfLists.maxOf { (me, elephant) ->
+//                "cache: $cache2".log()
+//                "me: $me".log()
+//                "elephant: $elephant".log()
+//                "".log()
+//                Thread.sleep(250L)
+
+                val meKey = me.hashCode()
+                val my = cache2[meKey] ?: map.solve(
+                    current,
+                    me,
+                    26,
+                    cache
+                ).also { cache2[meKey] = it }
+
+                val elephantKey = elephant.hashCode()
+                val elephants = cache2[elephantKey] ?: map.solve(
+                    current,
+                    elephant,
+                    26,
+                    cache
+                ).also { cache2[elephantKey] = it }
+
+                my + elephants
+            }
         }
-        part2 test 1 expect Unit
+        part2 test 1 expect 1707
     }
+}
+
+private fun create(l: List<Vertex>): List<Pair<List<Vertex>, List<Vertex>>> {
+    val list = mutableListOf<Pair<List<Vertex>, List<Vertex>>>()
+    val flags = BooleanArray(l.size)
+    var i = 0
+    fun inv(i: Int): Boolean {
+        flags[i] = !flags[i]
+        return flags[i]
+    }
+    while (i != l.size) {
+        val a: ArrayList<Vertex> = ArrayList()
+        val b: ArrayList<Vertex> = ArrayList()
+        for (j in l.indices) if (flags[j]) a.add(l[j]) else b.add(l[j])
+        list.add(a to b)
+        i = 0
+
+        while (i < l.size && !inv(i)) {
+            i++
+        }
+    }
+    return list
 }
 
 private fun Map<String, Vertex>.solve(
@@ -41,9 +107,7 @@ private fun Map<String, Vertex>.solve(
     remaining: Int,
     cache: MutableMap<String, Int>
 ): Int {
-//    "Current:${current.name} time:$remaining left:$left".log()
     if (remaining <= 0) {
-        "time ran out".log()
         return 0
     }
 
@@ -52,11 +116,6 @@ private fun Map<String, Vertex>.solve(
     if (current.rate > 0) {
         expectedPressure = (remaining - 1) * current.rate
         remaining -= 1
-    }
-
-    if (left.isEmpty()) {
-//        "".log()
-        return expectedPressure
     }
 
     if (left.none { cache.getValue(key(it, current)) + 1 <= remaining }) {
