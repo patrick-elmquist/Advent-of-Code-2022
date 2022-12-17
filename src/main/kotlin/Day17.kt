4dea796f79dc176a68733ebef020d657a8a78207
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2022-12-18 00:26:51 +0100

    Improved but not good

--- src/main/kotlin/Day17.kt
+++ src/main/kotlin/Day17.kt
@@ -9,146 +9,117 @@ import kotlin.math.abs
 
 fun main() {
     day(n = 17) {
-        part1(expected = 3114) { input ->
-            var shape = Shape.Minus
-            var position = Point(2, -4)
-            val winds = input.parseGasJets()
-            var windIndex = 0
-            val mass = List(7) { Point(it, 0) }.toMutableSet()
-            var rock = 1
-            var moveSide = true
-            while (rock < 2023) {
-                var blocked = false
-                if (moveSide) {
-                    val wind = winds[windIndex % winds.size]
+        val columns = 7
+        val startPoint = Point(2, -4)
+        val floor = List(columns) { Point(it, 0) }
+        val rocks = listOf(
+            Rock(Point(0, 0), Point(1, 0), Point(2, 0), Point(3, 0)),
+            Rock(Point(1, 0), Point(0, -1), Point(1, -1), Point(2, -1), Point(1, -2)),
+            Rock(Point(0, 0), Point(1, 0), Point(2, 0), Point(2, -1), Point(2, -2)),
+            Rock(Point(0, -3), Point(0, -2), Point(0, -1), Point(0, 0)),
+            Rock(Point(0, 0), Point(1, 0), Point(0, -1), Point(1, -1))
+        )
 
-                    val newX = (position.x + wind).coerceIn(0, 7 - shape.width)
-                    val newPosition = position.copy(x = newX)
-                    if (shape.translate(newPosition).none { it in mass }) {
-                        position = newPosition
-                    }
-                    windIndex++
-                    moveSide = false
-                } else {
-                    val newPosition = position.copy(y = position.y + 1)
-                    if (shape.translate(newPosition).none { it in mass }) {
-                        position = newPosition
-                    } else {
-                        blocked = true
-                    }
-                    moveSide = true
+        part1(expected = 3114) { input ->
+            val jets = input.parseGasJets()
+            var rock = rocks.first()
+            var bottomLeft = startPoint
+            var windIndex = 0
+            val ground = floor.toMutableSet()
+            var rockCount = 1
+            while (rockCount < 2023) {
+                val jetOffset = jets[windIndex++ % jets.size]
+
+                val newX = (bottomLeft.x + jetOffset).coerceIn(0, columns - rock.width)
+                val newPositionX = bottomLeft.copy(x = newX)
+                if (rock.translated(newPositionX).none { it in ground }) {
+                    bottomLeft = newPositionX
                 }
 
-                if (blocked) {
-                    mass += shape.translate(position)
-                    shape = shape.next()
-                    position = Point(x = 2, y = mass.minOf { it.y } - 4)
-                    rock++
-                    moveSide = true
+                val newPosition = bottomLeft.copy(y = bottomLeft.y + 1)
+                if (rock.translated(newPosition).none { it in ground }) {
+                    bottomLeft = newPosition
+                } else {
+                    ground += rock.translated(bottomLeft)
+                    rock = rocks[rockCount % rocks.size]
+                    rockCount++
+                    bottomLeft = startPoint + Point(0, ground.minOf { it.y })
                 }
             }
 
-            abs(mass.minOf { it.y.toFloat() }.toInt())
+            abs(ground.minOf { it.y.toFloat() }.toInt())
         }
         part1 test 1 expect 3068
 
-        part2(expected = 1540804597682) { input ->
-            var shape = Shape.Minus
-            var position = Point(2, -4)
-            val winds = input.parseGasJets()
-            var windIndex = 0
+        part2(expected = 1540804597682L) { input ->
+            val jets = input.parseGasJets()
+            var rock = rocks.first()
+            var position = startPoint
+            var jetIndex = 0
 
             val states = mutableMapOf<String, Int>()
             val heights = mutableListOf<Int>()
-            val mass = List(7) { Point(it, 0) }.toMutableSet()
-            var rock = 1
-            var moveSide = true
-            var indexWhenStarting = 0
+            val ground = floor.toMutableSet()
+            var rockCount = 1
+            var shapeInitialJetIndex = 0
             while (true) {
-                var blocked = false
-                if (moveSide) {
-                    val wind = winds[windIndex % winds.size]
+                val jetOffset = jets[jetIndex++ % jets.size]
 
-                    val newX = (position.x + wind).coerceIn(0, 7 - shape.width)
-                    val newPosition = position.copy(x = newX)
-                    if (shape.translate(newPosition).none { it in mass }) {
-                        position = newPosition
-                    }
-                    windIndex++
-                    moveSide = false
-                } else {
-                    val newPosition = position.copy(y = position.y + 1)
-                    if (shape.translate(newPosition).none { it in mass }) {
-                        position = newPosition
-                    } else {
-                        blocked = true
-                    }
-                    moveSide = true
+                val newX = (position.x + jetOffset).coerceIn(0, columns - rock.width)
+                val newPositionX = position.copy(x = newX)
+                if (rock.translated(newPositionX).none { it in ground }) {
+                    position = newPositionX
                 }
 
-                if (blocked) {
-                    if (rock > 250) {
-                        val key = key(shape, indexWhenStarting)
+                val newPosition = position.copy(y = position.y + 1)
+                if (rock.translated(newPosition).none { it in ground }) {
+                    position = newPosition
+                } else {
+                    if (rockCount > 250) {
+                        val key = key(rock, shapeInitialJetIndex)
                         if (key in states) {
                             break
                         } else {
-                            states += key to rock
+                            states += key to rockCount
                         }
                     }
-                    mass += shape.translate(position)
-                    val height = abs(mass.minOf { it.y.toFloat() }.toInt())
+                    ground += rock.translated(position)
+                    val height = abs(ground.minOf { it.y.toFloat() }.toInt())
                     heights.add(height)
-                    shape = shape.next()
-                    position = Point(x = 2, y = mass.minOf { it.y } - 4)
-                    rock++
-                    moveSide = true
-                    indexWhenStarting = windIndex % winds.size
+                    rock = rocks[rockCount % rocks.size]
+                    position = startPoint + Point(0, ground.minOf { it.y })
+                    rockCount++
+                    shapeInitialJetIndex = jetIndex % jets.size
                 }
             }
 
-            val key = key(shape, indexWhenStarting)
+            val key = key(rock, shapeInitialJetIndex)
             val rockNumber = states.getValue(key)
             val heightBeforePattern = heights[rockNumber - 2]
             val other = heights.last()
             val heightPerPattern = other - heightBeforePattern
 
-            val bigint = 1_000_000_000_000
-            val mid = bigint - (rockNumber - 1)
+            val mid = 1_000_000_000_000L - (rockNumber - 1)
 
             val countPerPattern = heights.drop(rockNumber - 1).count()
             val coveredByPattern = mid / countPerPattern
             val after = mid % countPerPattern
 
-            val t = heights.drop(rockNumber - 2)
+            val remaining = heights.drop(rockNumber - 2)
                 .take(after.toInt() + 1).let { it.last() - it.first() }
 
-            heightBeforePattern + coveredByPattern * heightPerPattern + t
+            heightBeforePattern + coveredByPattern * heightPerPattern + remaining
         }
-        part2 test 1 expect 1514285714288
+        part2 test 1 expect 1514285714288L
     }
 }
 
 private fun Input.parseGasJets() = single().map { if (it == '<') -1 else 1 }
 
-private fun key(shape: Shape, index: Int): String = shape.name + index
+private fun key(shape: Rock, index: Int): String = "$shape $index"
 
-private fun Shape.translate(position: Point): List<Point> = points.map { it + position }
-
-private fun Shape.next() = when (this) {
-    Shape.Minus -> Shape.Plus
-    Shape.Plus -> Shape.Corner
-    Shape.Corner -> Shape.Line
-    Shape.Line -> Shape.Square
-    Shape.Square -> Shape.Minus
-}
-
-private enum class Shape(val points: List<Point>) {
-    Minus(listOf(Point(0, 0), Point(1, 0), Point(2, 0), Point(3, 0))),
-    Plus(listOf(Point(1, 0), Point(0, -1), Point(1, -1), Point(2, -1), Point(1, -2))),
-    Corner(listOf(Point(0, 0), Point(1, 0), Point(2, 0), Point(2, -1), Point(2, -2))),
-    Line(listOf(Point(0, -3), Point(0, -2), Point(0, -1), Point(0, 0))),
-    Square(listOf(Point(0, 0), Point(1, 0), Point(0, -1), Point(1, -1)));
-
-    val width = points.maxOf { it.x } - points.minOf { it.x } + 1
-
+private data class Rock(val points: List<Point>) {
+    constructor(vararg points: Point) : this(points.toList())
+    val width get() = points.maxOf { it.x } - points.minOf { it.x } + 1
+    fun translated(position: Point): List<Point> = points.map { it + position } // should perhaps return a rock?
 }
