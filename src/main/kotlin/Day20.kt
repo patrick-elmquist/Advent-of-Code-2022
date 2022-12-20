c14a1606decee26f013a26233ab8533940d665e6
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2022-12-20 10:19:36 +0100

    Better but not great

--- src/main/kotlin/Day20.kt
+++ src/main/kotlin/Day20.kt
@@ -1,187 +1,114 @@
 import day.day
-import day.toInts
-import day.toLongs
-import util.log
 import kotlin.math.abs
 
 // answer #1: 7395
-// answer #2:
+// answer #2: 1640221678213
 
 fun main() {
     day(n = 20) {
         part1(expected = 7395L) { input ->
-            val og = input.lines
-
-            val ints = input.toLongs()
-            val first = og.first().toLong()
-            val headId = "0 $first"
-            val head = Node(
-                id = headId,
-                value = first,
-            )
+            val longs = input.lines.map(String::toLong)
+            val first = longs.first()
+            val head = Node(value = first)
             var prev = head
-            val idsInOrder = ints.mapIndexed { index, i ->
+            val idsInOrder = longs.mapIndexed { index, i ->
                 "$index $i"
             }
-            val idNodeMap = ints.drop(1).mapIndexed { index, i ->
+            val idNodeMap = longs.drop(1).mapIndexed { index, i ->
                 val id = "${index + 1} $i"
-                id to Node(id = id, value = i).also {
+                id to Node(i).also {
                     prev.next = it
                     it.prev = prev
                     prev = it
                 }
             }.toMap().toMutableMap()
-            idNodeMap[headId] = head
+            idNodeMap["0 $first"] = head
             prev.next = head
             head.prev = prev
 
-            solve2(head, idsInOrder, idNodeMap)
-            var index1 = Long.MIN_VALUE
-            var index2 = Long.MIN_VALUE
-            var index3 = Long.MIN_VALUE
-            var i = 1
-            var current = idNodeMap.values.first { it.value == 0L }.log("node 0")
-            while (i <= 3001) {
-                current = current.forward
-                if (i == 1000) {
-                    index1 = current.value
-                }
-                if (i == 2000) {
-                    index2 = current.value
-                }
-                if (i == 3000) {
-                    index3 = current.value
-                }
-                i++
-            }
-            index1.log("1000th:") + index2.log("2000th:") + index3.log("3000th:")
+            solve2(idsInOrder, idNodeMap)
+            findResult(idNodeMap)
         }
-//        part1 test 2 expect 10
         part1 test 1 expect 3L
 
-        part2 { input ->
-            val og = input.lines
-            val key = 811589153L
-            val ints = input.toLongs().map {
-                it * key
-            }
-            val first = ints.first()
-            val headId = "0 $first"
-            val head = Node(
-                id = headId,
-                value = first,
-            )
+        part2(expected = 1640221678213L) { input ->
+            val longs = input.lines.map(String::toLong)
+                .map { it * 811589153L }
+            val first = longs.first()
+            val head = Node(first)
             var prev = head
-            val idsInOrder = ints.mapIndexed { index, i ->
+            val idsInOrder = longs.mapIndexed { index, i ->
                 "$index $i"
             }
-            val idNodeMap = ints.drop(1).mapIndexed { index, i ->
+            val idNodeMap = longs.drop(1).mapIndexed { index, i ->
                 val id = "${index + 1} $i"
-                id to Node(id = id, value = i).also {
+                id to Node(i).also {
                     prev.next = it
                     it.prev = prev
                     prev = it
                 }
             }.toMap().toMutableMap()
-            idNodeMap[headId] = head
+            idNodeMap["0 $first"] = head
             prev.next = head
             head.prev = prev
 
-            print(head)
             repeat(10) {
-                solve2(head, idsInOrder, idNodeMap)
+                solve2(idsInOrder, idNodeMap)
             }
-            var index1 = Long.MIN_VALUE
-            var index2 = Long.MIN_VALUE
-            var index3 = Long.MIN_VALUE
-            var i = 1
-            var current = idNodeMap.values.first { it.value == 0L }.log("node 0")
-            while (i <= 3001) {
-                current = current.forward
-                if (i == 1000) {
-                    index1 = current.value
-                }
-                if (i == 2000) {
-                    index2 = current.value
-                }
-                if (i == 3000) {
-                    index3 = current.value
-                }
-                i++
-            }
-            index1.log("1000th:") + index2.log("2000th:") + index3.log("3000th:")
+            findResult(idNodeMap)
         }
         part2 test 1 expect 1623178306L
     }
 }
 
-private fun solve2(head: Node, idsInOrder: List<String>, idNodeMap: Map<String, Node>) {
+private fun findResult(idNodeMap: MutableMap<String, Node>): Long =
+    buildList {
+        var current = idNodeMap.values.first { it.value == 0L }
+        for (i in 1..3001) {
+            current = current.forward
+            if (i == 1000 || i == 2000 || i == 3000) {
+                add(current.value)
+            }
+        }
+    }.sum()
+
+private fun solve2(idsInOrder: List<String>, idNodeMap: Map<String, Node>) {
+    val adjustedSize = idsInOrder.size - 1
     idsInOrder.forEach { id ->
         val node = idNodeMap.getValue(id)
+        if (node.value == 0L) return@forEach
+        var prev = if (node.value < 0) node.back else node.forward
+        node.forward.prev = node.back
+        node.back.next = node.forward
         when {
             node.value < 0 -> {
-                var prev = node.back
-                node.forward.prev = node.back
-                node.back.next = node.forward
-                repeat((abs(node.value) % (idsInOrder.size - 1)).toInt()) {
+                repeat((abs(node.value) % adjustedSize).toInt()) {
                     prev = prev.back
                     if (prev == node) {
                         prev = prev.back
                     }
                 }
-                val nHead = prev.forward
-                prev.next = node
-                node.prev = prev
-                nHead.prev = node
-                node.next = nHead
             }
 
-            node.value > 0 -> {
-                var prev = node.forward
-                node.forward.prev = node.back
-                node.back.next = node.forward
-                repeat(((abs(node.value) - 1) % (idsInOrder.size - 1)).toInt()) {
+            else -> {
+                repeat(((abs(node.value) - 1) % adjustedSize).toInt()) {
                     prev = prev.forward
                 }
-                val nHead = prev.forward
-                prev.next = node
-                node.prev = prev
-                nHead.prev = node
-                node.next = nHead
             }
-
-            else -> return@forEach
         }
-//        print(head)
-//        println()
+        val nHead = prev.forward
+        prev.next = node
+        node.prev = prev
+        nHead.prev = node
+        node.next = nHead
     }
-
-    print(idNodeMap.values.first { it.value == 0L })
-}
-
-private fun print(head: Node) {
-    var current = head
-    var start = true
-    while (start || current != head) {
-        print(current.value)
-        print(", ")
-        current = current.next!!
-        start = false
-    }
-    println()
 }
 
 private class Node(
-    val id: String,
     val value: Long,
     var prev: Node? = null,
     var next: Node? = null
 ) {
-    val forward get() = next!!
-    val back get() = prev!!
-    override fun toString(): String {
-        return buildString {
-            append("Node(value:$value prev:${prev!!.value} next:${next!!.value})")
-        }
-    }
+    val forward get() = requireNotNull(next)
+    val back get() = requireNotNull(prev)
 }
