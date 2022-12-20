8cbcbdeb98039691db7513cbfb91aed07760a0f5
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2022-12-20 13:26:46 +0100

    Cleanup

--- src/main/kotlin/Day20.kt
+++ src/main/kotlin/Day20.kt
@@ -7,58 +7,57 @@ import kotlin.math.abs
 fun main() {
     day(n = 20) {
         part1(expected = 7395L) { input ->
-            val longs = input.lines.map(String::toLong)
-            val idNodeMap = idToNodeMap(longs)
-            val idsInOrder = idNodeMap.keys.map(String::toInt).sorted().map(Int::toString)
-            solve(idsInOrder, idNodeMap)
+            val idNodeMap = idToNodeMap(input.lines.map(String::toLong))
+            val idsInOrder = idNodeMap.keys.sorted()
+
+            performMixing(idsInOrder, idNodeMap)
+
             findResult(idNodeMap)
         }
         part1 test 1 expect 3L
 
         part2(expected = 1640221678213L) { input ->
-            val longs = input.lines.map(String::toLong).map { it * 811589153L }
-            val idNodeMap = idToNodeMap(longs)
-            val idsInOrder = idNodeMap.keys.map(String::toInt).sorted().map(Int::toString)
+            val multipliedInput = input.lines.map(String::toLong).map { it * 811589153L }
+            val idNodeMap = idToNodeMap(multipliedInput)
+            val idsInOrder = idNodeMap.keys.sorted()
 
-            repeat(10) {
-                solve(idsInOrder, idNodeMap)
-            }
+            repeat(times = 10) { performMixing(idsInOrder, idNodeMap) }
+
             findResult(idNodeMap)
         }
         part2 test 1 expect 1623178306L
     }
 }
 
-private fun idToNodeMap(longs: List<Long>): MutableMap<String, Node> {
-    val first = longs.first()
-    val head = Node(first)
-    var prev = head
-    val idNodeMap = longs.drop(1).mapIndexed { index, i ->
-        val id = "${index + 1}"
-        id to Node(i).also {
-            prev.next = it
+private fun idToNodeMap(longs: List<Long>): Map<Int, Node> {
+    var prev: Node? = null
+    val idNodeMap = longs.mapIndexed { index, i ->
+        index to Node(i).also {
+            prev?.next = it
             it.prev = prev
             prev = it
         }
-    }.toMap().toMutableMap()
-    idNodeMap["0"] = head
-    prev.next = head
+    }.toMap()
+    val head = idNodeMap.getValue(0)
+    prev?.next = head
     head.prev = prev
     return idNodeMap
 }
 
-private fun findResult(idNodeMap: MutableMap<String, Node>): Long =
-    buildList {
-        var current = idNodeMap.values.first { it.value == 0L }
-        for (i in 1..3000) {
-            current = current.forward
-            if (i == 1000 || i == 2000 || i == 3000) {
-                add(current.value)
-            }
-        }
-    }.sum()
+private fun findResult(idNodeMap: Map<Int, Node>): Long =
+    forwardSequence(idNodeMap.values.first { it.value == 0L })
+        .take(3)
+        .map(Node::value)
+        .reduce(Long::plus)
 
-private fun solve(idsInOrder: List<String>, idNodeMap: Map<String, Node>) {
+private fun forwardSequence(head: Node) =
+    generateSequence(head) { node ->
+        var current = node
+        repeat(1000) { current = current.forward }
+        current
+    }.drop(1)
+
+private fun performMixing(idsInOrder: List<Int>, idNodeMap: Map<Int, Node>) {
     val adjustedSize = idsInOrder.size - 1
     idsInOrder.forEach { id ->
         val node = idNodeMap.getValue(id)
@@ -66,16 +65,13 @@ private fun solve(idsInOrder: List<String>, idNodeMap: Map<String, Node>) {
         var prev = if (node.value < 0) node.back else node.forward
         node.forward.prev = node.back
         node.back.next = node.forward
-        val absoluteMovementd = abs(node.value)
+        val absoluteMovement = abs(node.value)
         when {
-            node.value < 0 -> repeat((absoluteMovementd % adjustedSize).toInt()) {
+            node.value < 0 -> repeat((absoluteMovement % adjustedSize).toInt()) {
                 prev = prev.back
-                if (prev == node) {
-                    prev = prev.back
-                }
             }
 
-            else -> repeat(((absoluteMovementd - 1) % adjustedSize).toInt()) {
+            else -> repeat(((absoluteMovement - 1) % adjustedSize).toInt()) {
                 prev = prev.forward
             }
         }
