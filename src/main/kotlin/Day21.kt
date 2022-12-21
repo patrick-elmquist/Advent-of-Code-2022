495dd8eb7fefd163696659c033ddfac736057d56
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2022-12-21 10:57:10 +0100

    Cleaned up 21

--- src/main/kotlin/Day21.kt
+++ src/main/kotlin/Day21.kt
@@ -7,87 +7,133 @@ import day.day
 fun main() {
     day(n = 21) {
         part1(expected = 41857219607906L) { input ->
-            val monkeys = input.parseMonkeys()
+            val monkeys = input.parseMonkeys().toMutableMap()
             val root = monkeys.getValue("root")
-            root.tryResolve(monkeys.toMutableMap()) ?: error("")
+            root.tryResolve(monkeys) ?: error("should be able to resolve")
         }
         part1 test 1 expect 152L
 
         part2(expected = 3916936880448L) { input ->
-            val me = "humn"
-            val monkeys = input.parseMonkeys().toMutableMap()
+            val monkeys = input.parseMonkeys()
+                .toMutableMap()
+            monkeys.remove("humn")
 
             val root = monkeys.getValue("root") as Number.Expression
-            monkeys.remove("root")
-            monkeys.remove(me)
+            root.tryResolve(monkeys)
 
-            val a = monkeys.getValue(root.a)
-            val b = monkeys.getValue(root.b)
+            val leftBranch = monkeys.getValue(root.left)
+            val rightBranch = monkeys.getValue(root.right)
 
-            a.tryResolve(monkeys)
+            when {
+                leftBranch is Number.Raw ->
+                    solveEquation(
+                        endResult = leftBranch.n,
+                        firstExpression = rightBranch as Number.Expression,
+                        monkeys = monkeys
+                    )
 
-            var start = a
-            var right = b.tryResolve(monkeys)
-                ?: error("could not resolve b")
-            while (right != 0L) {
-                val current = start as Number.Expression
-
-                if (current.a == me) {
-                    val raw = (monkeys.getValue(current.b) as Number.Raw).n
-                    when (current.command) {
-                        "+" -> right -= raw
-                        "-" -> right += raw
-                        "/" -> right *= raw
-                        "*" -> right /= raw
-                    }
-                    break
+                rightBranch is Number.Raw -> {
+                    solveEquation(
+                        endResult = rightBranch.n,
+                        firstExpression = leftBranch as Number.Expression,
+                        monkeys = monkeys
+                    )
                 }
-                if (current.b == me) {
-                    val raw = (monkeys.getValue(current.a) as Number.Raw).n
-                    when (current.command) {
-                        "+" -> right -= raw
-                        "-" -> right -= raw
-                        "/" -> right = raw / right
-                        "*" -> right /= raw
-                    }
-                    break
-                }
-
-                val ca = monkeys.getValue(current.a)
-                val cb = monkeys.getValue(current.b)
-
-                val (raw, expression) = when {
-                    ca is Number.Raw -> ca.n to cb
-                    cb is Number.Raw -> cb.n to ca
-                    else -> error("")
-                }
-
-                when (current.command) {
-                    "+" -> right -= raw
-                    "-" -> {
-                        if (ca is Number.Raw) {
-                            right -= raw
-                            right *= -1
-                        } else {
-                            right += raw
-                        }
-                    }
-
-                    "/" -> {
-                        if (ca is Number.Raw) {
-                            right = raw / right
-                        } else {
-                            right *= raw
-                        }
-                    }
-
-                    "*" -> right /= raw
-                }
-                start = expression
+                else -> error("")
             }
-            right
         }
         part2 test 1 expect 301L
+    }
+}
+
+private fun solveEquation(
+    endResult: Long,
+    firstExpression: Number,
+    monkeys: MutableMap<String, Number>
+): Long {
+    var leftSide = endResult
+    var nextExpression = firstExpression
+    while (true) {
+        val current = nextExpression as Number.Expression
+
+        val leftBranch = monkeys[current.left]
+        val rightBranch = monkeys[current.right]
+
+        if (leftBranch == null) {
+            val raw = (rightBranch as Number.Raw).n
+            when (current.command) {
+                "+" -> leftSide -= raw
+                "*" -> leftSide /= raw
+                "-" -> leftSide += raw
+                "/" -> leftSide *= raw
+            }
+            return leftSide
+        }
+
+        if (rightBranch == null) {
+            val raw = (leftBranch as Number.Raw).n
+            when (current.command) {
+                "+" -> leftSide -= raw
+                "*" -> leftSide /= raw
+                "-" -> leftSide -= raw
+                "/" -> leftSide = raw / leftSide
+            }
+            return leftSide
+        }
+
+        val (raw, expression) = when {
+            leftBranch is Number.Raw -> leftBranch.n to rightBranch
+            rightBranch is Number.Raw -> rightBranch.n to leftBranch
+            else -> error("")
+        }
+
+        when (current.command) {
+            "+" -> leftSide -= raw
+            "*" -> leftSide /= raw
+            "-" -> {
+                if (leftBranch is Number.Raw) {
+                    leftSide -= raw
+                    leftSide *= -1
+                } else {
+                    leftSide += raw
+                }
+            }
+
+            "/" -> {
+                if (leftBranch is Number.Raw) {
+                    leftSide = raw / leftSide
+                } else {
+                    leftSide *= raw
+                }
+            }
+        }
+        nextExpression = expression
+    }
+}
+
+private sealed class Number {
+    abstract fun tryResolve(monkeys: MutableMap<String, Number>): Long?
+
+    data class Expression(val left: String, val right: String, val command: String) : Number() {
+        override fun tryResolve(monkeys: MutableMap<String, Number>): Long? {
+            val leftResolved = monkeys.tryResolveAndMap(left)
+            val rightResolved = monkeys.tryResolveAndMap(right)
+            if (leftResolved == null || rightResolved == null) return null
+            return when (command) {
+                "+" -> leftResolved + rightResolved
+                "-" -> leftResolved - rightResolved
+                "/" -> leftResolved / rightResolved
+                "*" -> leftResolved * rightResolved
+                else -> error("")
+            }
+        }
+
+        private fun MutableMap<String, Number>.tryResolveAndMap(name: String) =
+            get(name)?.tryResolve(this)?.also { this[name] = Raw(it) }
+    }
+
+    data class Raw(val n: Long) : Number() {
+        override fun tryResolve(monkeys: MutableMap<String, Number>): Long = n
     }
 }
 
@@ -95,49 +141,8 @@ private fun Input.parseMonkeys(): Map<String, Number> =
     lines.associate {
         val split = it.split(" ")
         split.first().dropLast(1) to if (split.size == 2) {
-            val n = split.last().toLong()
-            Number.Raw(n)
+            Number.Raw(split[1].toLong())
         } else {
-            val a = split[1]
-            val sign = split[2]
-            val b = split[3]
-            Number.Expression(a, b, sign)
+            Number.Expression(split[1], split[3], split[2])
         }
     }
-
-private sealed class Number {
-    abstract fun tryResolve(monkeys: MutableMap<String, Number>): Long?
-
-    data class Expression(val a: String, val b: String, val command: String) : Number() {
-        override fun tryResolve(monkeys: MutableMap<String, Number>): Long? {
-            val aInt = tryResolveAndCache(monkeys, a)
-            val bInt = tryResolveAndCache(monkeys, b)
-            if (aInt == null || bInt == null) return null
-            return when (command) {
-                "+" -> aInt + bInt
-                "-" -> aInt - bInt
-                "/" -> aInt / bInt
-                "*" -> aInt * bInt
-                else -> error("")
-            }
-        }
-
-        private fun tryResolveAndCache(monkeys: MutableMap<String, Number>, a: String): Long? {
-            val aInt = when (val monkeyA = monkeys[a]) {
-                null -> null
-                is Expression -> {
-                    monkeyA.tryResolve(monkeys)?.also { monkeys[a] = Raw(it) }
-                }
-
-                is Raw -> {
-                    monkeyA.tryResolve(monkeys)
-                }
-            }
-            return aInt
-        }
-    }
-
-    data class Raw(val n: Long) : Number() {
-        override fun tryResolve(monkeys: MutableMap<String, Number>): Long = n
-    }
-}
